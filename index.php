0dd50625fcd21a1e9366181c4b771f2c08b13527
pranavj1001 <pranavj1001@gmail.com>
2017-01-01 01:10:23 +0530

    add some new files

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+    include("controller.php");
+    include("views/header.php");
+    include("views/home.php");
+    include("views/footer.php");
+
+?>
---
+++ views/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+  </head>
+  <body>
