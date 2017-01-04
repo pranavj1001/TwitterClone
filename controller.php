043a7b0ac7f9c3c109514b9579b835d4d5d46b33
pranavj1001 <pranavj1001@gmail.com>
2017-01-05 00:52:43 +0530

    update function displayTweets()

--- controller.php
+++ controller.php
@@ -16,11 +16,25 @@
     }
 
     function displayTweets($type){
+        
+        global $link;
+        
         if($type == "public"){
             $whereClause = "";
         }
         
         $query = "SELECT * FROM tweets ".$whereClause." ORDER BY `datetime` DESC";
+        
+        $result = mysqli_query($link, $query);
+        
+        if(mysqli_num_rows($result) == 0){
+            echo "There are no tweets right now to show. Why don't you start tweeting? :)";
+        }else{
+            while($row == mysqli_fetch_assoc($result)){
+                $userQuery = "SELECT * FROM users WHERE id = ".mysqli_real_escape_string($link, $row['userid'])." LIMIT 1";
+                $userQueryResult = mysqli_query($link, $userQuery);
+            }
+        }
         
     }
     
